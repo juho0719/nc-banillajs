b983f1a94adf60984c26bf541e33b0378e5b1b0e
juho0719 <juho0719@wemakeprice.com>
2020-10-18 06:40:03 +0900

    username saving added

---
+++ js/greetings.js
@@ -0,0 +1,38 @@
+import "../css/style.css";
+
+cosnt form = document.querySelector(".js-form"),
+	input = form.querySelector("input"),
+	greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "user",
+	ACTIVE = "active";
+
+function askForName() {
+	greeting.classList.remove(ACTIVE);
+	form.classList.add(ACTIVE);
+	input.value = "";
+}
+
+function paintGreeting(text) {
+	form.classList.remove(ACTIVE);
+	greeting.classList.add(ACTIVE);
+	greeting.innerText = `Hello ${text}`;
+}
+
+function init() {
+	const name = localStorage.getItem(USER_LS);
+	if (name === null) {
+		askForName();
+	} else {
+		paintGreeting(name);
+	}
+}
+
+function saveName(event) {
+	event.preventDefault();
+	localStorage.setItem(USER_LS, input.value);
+	paintGreeting(input.value);
+}
+
+form.addEventListner("submit", saveName);
+init();
