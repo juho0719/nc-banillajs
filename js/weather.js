aa85196bb476b9c4fcec447b27d40e09858cdb9f
Juho Kim <juho0719@naver.com>
2020-10-19 05:07:41 +0900

    geographic complete

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,18 @@
-
+const weather = document.querySelector(".js-weather");
+const API_KEY = "4e64eaa8fe03029e882d288356c63b51";
 const COORDS = "coords";
+
+function getWeather(latitude, longitude) {
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}appid=${API_KEY}&units=metric`
+	).then(function (response) {
+		return response.json();
+	}).then(function (json) {
+		const temperature = json.mail.temp;
+		const place = json.name;
+		weather.innerText = `${temperature} @ ${place}`;
+	});
+}
 
 function saveCoords(coordsObj) {
 	localStorage.setItem(COORDS, coordsObj);
@@ -13,6 +26,7 @@ function handleGeoSuccess(position) {
 		longitude
 	};
 	saveCoords(coordsObj);
+	getWeather(latitude, longitude);
 }
 
 function handleGeoError(position) {
@@ -28,7 +42,8 @@ function loadCorods() {
 	if (loadCoords === null) {
 		askForCoords();
 	} else {
-
+		const parseCoords = JSON.parse(loadedCoords);
+		getWeather(parseCoords.latitude, parseCoords.longitude);
 	}
 }
 
